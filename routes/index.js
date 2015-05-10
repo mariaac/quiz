a3530b67f548a61413406dab0833aa4d1b1c1bfa
mariaac <maria.alvarez.custodio@alumnos.upm.es>
2015-05-10 17:26:47 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
+//Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
