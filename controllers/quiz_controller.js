8eca610b60acb0b8c7cf7efcca3295e2d11716db
mariaac <maria.alvarez.custodio@alumnos.upm.es>
2015-05-11 15:17:42 +0200

    Cajetin de busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,9 +16,26 @@ exports.load = function(req, res, next, quizId){
 
 //GET /quizes
 exports.index = function(req, res){
-	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
-	}).catch(function(error){next(error);})
+	if(req.query.search != null){
+		req.query.search = '%'+req.query.search+'%';
+		models.Quiz.findAll({where: ["pregunta like ?", req.query.search]}).then(function(quizes){
+			var i;
+			for (i=1; i<quizes.length; i++){
+				while(quizes[i].pregunta<quizes[i-1].pregunta){
+					var aux = quizes[i-1];
+					quizes[i-1]=quizes[i];
+					quizes[i]=aux;
+				}
+			}
+			res.render('quizes/index', {quizes: quizes});	
+		}).catch(function(error){next(error);})
+	}
+	else {
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index', {quizes: quizes});
+		}).catch(function(error){next(error);})
+	}
+	
 };
 
 //GET /quizes/:id
@@ -34,3 +51,13 @@ exports.answer = function(req,res){
 	} 
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
+
+//GET  /quizes?search=texto_a_buscar
+
+
+
+
+
+
+
+
