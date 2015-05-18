a8b1431ba95dec443ac41b4183a783cab23776a3
mariaac <maria.alvarez.custodio@alumnos.upm.es>
2015-05-18 10:00:54 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function(req, res) {
 });
 
 //Autoload de comandos con :quizId
-router.param('quizId', quizController.load);
+router.param('quizId', quizController.load);//autoload: quizId
+router.param('commentId', commentController.load);//autoload: commentId
 
 //Definicion de rutas de sesion
 router.get('/login', sessionController.new); //formulario login
@@ -30,7 +31,9 @@ router.get('/author', function(req, res){
 	res.render('author', {errors: []});
 });
 
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
