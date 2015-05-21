7176723a94bf97872367034cf5125c984e6c2e88
mariaac <maria.alvarez.custodio@alumnos.upm.es>
2015-05-21 09:58:34 +0200

    HTTPS - HTTP seguro

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
+var https = require("https");
+var fs = require("fs");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
